6cb2b1e0c547ac5c9dafaaa20f1460b212b5c87b
Alexandre Nascimento <97413310+Alexandrexy1@users.noreply.github.com>
2024-07-13 12:48:30 -0300

    BufferedWriter

--- Day01/bufferedwriter/Main.java
+++ Day01/bufferedwriter/Main.java
@@ -14,6 +14,7 @@ public class Main {
                 bw.write(message);
                 bw.newLine();
             }
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
