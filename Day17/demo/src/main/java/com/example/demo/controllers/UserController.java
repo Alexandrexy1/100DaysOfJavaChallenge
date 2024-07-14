c5c8ada6fbbd9d05da1bd1eb508b306fe5e6b810
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-07-13 23:14:00 -0300

    User UserRepository, UserService and UserController

---
+++ Day17/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {         
+        userService.insert(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+}
+
---
+++ Day17/demo/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,48 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> object = repository.findById(id);
+		
+		return object.orElseThrow(() -> new IllegalArgumentException("id doesn't exists. Id " + id));
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
+
+	public void delete(Long id) {
+        if(repository.existsById(id)) {
+            repository.deleteById(id);				
+        } else throw new IllegalArgumentException("Id doesn't exists.");
+	}
+
+	public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateUser(entity, obj);
+        return repository.save(entity);
+	}
+
+	private void updateUser(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+	}
+}
