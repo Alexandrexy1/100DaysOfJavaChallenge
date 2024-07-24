15812466c55a36a787060f49c5fb7c9f145bf7d4
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-07-23 21:14:18 -0300

    api external

---
+++ Day27/study/src/main/java/com/example/study/client/configuration/WebClientConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.study.client.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+    @Bean
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
